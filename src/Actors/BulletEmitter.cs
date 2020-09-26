632c5d29e12d40110907cecd3b13b0276674d961
Aim4 <aimforking@gmail.com>
2020-09-26 19:22:04 -0400

    Add BulletEmitter to Enemy

---
+++ src/Actors/BulletEmitter.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public class BulletEmitter : Node2D
+{
+    PackedScene _bulletScene;
+    public override void _Ready()
+    {
+        _bulletScene = ResourceLoader.Load("res://src/Objects/Bullet.tscn") as PackedScene;
+    }
+
+    public override void _Process(float delta)
+    {
+        Rotate((float)0.1);
+
+        Bullet b = _bulletScene.Instance() as Bullet;
+        b.Position = new Vector2(100, 100);
+        b.Rotation = Rotation;
+        GetParent().GetParent().AddChild(b);
+    }
+}
---
+++ src/Objects/Bullet.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public class Bullet : Area2D
+{
+    public override void _Ready()
+    {
+        
+    }
+
+    public override void _Process(float delta)
+    {
+
+    }
+}
