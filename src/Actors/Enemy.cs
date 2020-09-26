6aa3338d6aa4c9bfab8149fc02d62598d31803af
Aim4 <aimforking@gmail.com>
2020-09-26 15:46:26 -0400

    Free Enemy when it leaves the screen

---
+++ src/Actors/Enemy.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public class Enemy : Area2D
+{
+
+    public override void _Ready()
+    {
+         
+    }
+
+    public override void _Process(float delta)
+    {
+
+    }
+
+    public void OnVisibilityNotifier2DScreenExited()
+    {
+        QueueFree();
+    }
+}
