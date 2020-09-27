5fb7ea324b7b42a94e9b3b358cd59e441f7b67c9
Aim4 <aimforking@gmail.com>
2020-09-27 16:59:58 -0400

    Add PlayerBullet

---
+++ src/Objects/PlayerBullet.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public class PlayerBullet : Area2D
+{
+    private int _speed = 100;
+
+    public override void _Ready()
+    {
+        
+    }
+
+    public override void _Process(float delta)
+    {
+        Vector2 velocity = new Vector2(0, _speed * delta);
+        this.Position += velocity;
+    }
+}
