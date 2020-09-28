da7f35d15dce0718e8b1c7d87f5ea4aaca9ef5a3
Aim4 <aimforking@gmail.com>
2020-09-28 16:05:35 -0400

    Add newgame and endgame signals when buttons pressed in GameOver UI

---
+++ src/UserInterface/GameOver.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public class GameOver : NinePatchRect
+{
+    [Signal]
+    public delegate void NewGame();
+    [Signal]
+    public delegate void EndGame();
+
+    public override void _Ready()
+    {
+        
+    }
+
+    public override void _Process(float delta)
+    {
+
+    }
+
+    public void OnYesButtonPressed()
+    {
+        EmitSignal(nameof(NewGame));
+    }
+
+    public void OnNoButtonPressed()
+    {
+        EmitSignal(nameof(EndGame));
+    }
+}
