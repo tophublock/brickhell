096405a764ff4e7e7ee13a0a8f40baf9fc3629e3
Aim4 <aimforking@gmail.com>
2020-10-02 15:51:26 -0400

    Load buttons to main menu script

---
+++ src/UserInterface/MainMenu.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public class MainMenu : CenterContainer
+{
+    private string _buttonContainerPath = "MainMenu/VBoxContainer/MarginContainer/VBoxContainer/";
+    private Button _startButton;
+    private Button _quitButton;
+
+    public override void _Ready()
+    {
+        _startButton = GetNode<Button>(_buttonContainerPath + "Start");
+        _quitButton = GetNode<Button>(_buttonContainerPath + "Quit");
+    }
+}
