97e9eb35f6f4290f5024bdfb9245d89a8d454190
Aim4 <aimforking@gmail.com>
2020-10-02 15:37:28 -0400

    Add Paused screen

--- src/Main/PauseNode.cs
+++ src/Main/PauseNode.cs
@@ -3,11 +3,26 @@ using System;
 
 public class PauseNode : Node
 {
+    private CenterContainer _pause;
+    public override void _Ready()
+    {
+        _pause = GetNode<CenterContainer>("Pause");
+        _pause.Hide();
+    }
+
     public override void _Input(InputEvent inputEvent)
     {
         if (inputEvent.IsActionPressed("ui_pause"))
         {
             GetTree().Paused = !GetTree().Paused;
+            if (GetTree().Paused)
+            {
+                _pause.Show();
+            }
+            else
+            {
+                _pause.Hide();
+            }
         }
     }
 }
