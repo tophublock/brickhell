6eef5472aa6315e8587eceba9132e9e36b54350e
Aim4 <aimforking@gmail.com>
2020-10-02 15:56:48 -0400

    Start game on start button press

--- src/UserInterface/MainMenu.cs
+++ src/UserInterface/MainMenu.cs
@@ -3,14 +3,9 @@ using System;
 
 public class MainMenu : CenterContainer
 {
-    private string _buttonContainerPath = "MainMenu/VBoxContainer/MarginContainer/VBoxContainer/";
-    private Button _startButton;
-    private Button _quitButton;
-
-    public override void _Ready()
+    public void OnStartPressed()
     {
-        _startButton = GetNode<Button>(_buttonContainerPath + "Start");
-        _quitButton = GetNode<Button>(_buttonContainerPath + "Quit");
+        GetTree().ChangeScene("res://src/Main/Game.tscn");
     }
 
     public void OnQuitPressed()
